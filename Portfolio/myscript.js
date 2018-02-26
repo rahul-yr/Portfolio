ae2d195d42caebc901191582cbf9d36f0df76ca1
Yarragodula Rahul Reddy <rahulreddy4119@gmail.com>
2018-02-26 10:15:36 +0530

    Portfolio First Commit

---
+++ Portfolio/myscript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  
+  
+ // $(".bdy").addClass("animated bounce");
+
+  
+});
+ function notReady(){
+   alert("Under Maintance!..Please use Email id. Mentioned below to share details..");
+ }
